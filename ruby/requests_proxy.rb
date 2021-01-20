64b15ae5c434929698da495363a857dffc198215
ProxyMesh <57071867+proxymesh@users.noreply.github.com>
2021-01-20 05:43:12 -0800

    don't need proxy_uri, show get request & response

--- ruby/requests_proxy.rb
+++ ruby/requests_proxy.rb
@@ -2,15 +2,15 @@ require 'net/http'
 require 'uri'
 
 uri = URI('http://example.com')
-proxy_uri = URI('http://host:port')
 proxy_options = {
-  p_addr: proxy_options.host,
-  p_port: proxy_options.port,
+  p_addr: 'PROXYHOST',
+  p_port: PORT,
   # username/password are optional if you have IP authentication
   p_user: 'USERNAME',
   p_pass: 'PASSWORD',
 }
 
 Net::HTTP.start(uri.host, uri.port, **proxy_options) do |http|
-  response = http.get('/')
+  request = Net::HTTP::Get.new uri
+  response = http.request request
 end
